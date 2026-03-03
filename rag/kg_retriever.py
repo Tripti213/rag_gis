2ed5accf3b6b7e15d11e8d2a972625bcb88abfd7
Kanka Oza <kankaoza1609@gmail.com>
2026-03-03 23:07:35 +0530

    implementing multihop

--- rag/kg_retriever.py
+++ rag/kg_retriever.py
@@ -1,39 +1,15 @@
-from rag.query_node_generation import QueryMapper
-
 class KGRetriever:
 
     def __init__(self, triples):
         self.triples=triples
 
+        #for forward adjacency
+        self.graph={}
 
-    def search(self, query):
+        #for rev adjacency
+        self.reverse_graph={}
 
-        mapper=QueryMapper()
-        concepts=mapper.map_query(query)
+        for s,r,o in triples:
 
-        matched_entities=set()
-
-        for s, r, o in self.triples:
-
-            for c in concepts:
-
-                if c in o.lower():
-                    matched_entities.add(s)
-
-                if c in r.lower():
-                    matched_entities.add(s)
-
-        return list(matched_entities)
-
-
-    def filter_by_location(self, entities, location):
-
-        valid=set()
-
-        for s, r, o in self.triples:
-
-            if r == "located_in" and o.lower() == location.lower():
-                if s in entities:
-                    valid.add(s)
-
-        return list(valid)
+            self.graph.setdefault(s.lower(),[]).append((r,o.lower()))
+            self.reverse_graph.setdefault(o.lower(),[]).append((r,s.lower()))
